177eb2bd8fa7ee0c8fa42a4796336d6f5bbe9363
Michaelnazaio <165970568+Michaelnazaio@users.noreply.github.com>
2024-10-03 22:09:49 -0300

    Add files via upload

---
+++ (25-09) aulas-frontend-pwa/src/comum/componentes/Principal/Principal.jsx
@@ -0,0 +1,12 @@
+import BotaoCustomizado from '../BotaoCustomizado/BotaoCustomizado';
+import './Principal.css';
+
+function Principal() {
+  return <main className='principal_root'>Principal
+    <BotaoCustomizado>Salvar</BotaoCustomizado>
+  </main>;
+
+  
+}
+
+export default Principal;
